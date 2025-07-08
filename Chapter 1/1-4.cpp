6ec20771a0bad0fa8b180ec9a14a1d79e305c5bb
cekl <3515375434@qq.com>
2025-07-08 17:43:16 +0800

    read Section 1.3

---
+++ Chapter 1/1-4.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    int v1 = 0, v2 = 0;
+    cin>> v1 >> v2;
+    cout << v1 * v2 << endl;
+    return 0;
+
+}
---
+++ Chapter 1/1-5.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    cout << "请输入两个数字：" ;
+    cout << endl;
+    int v1 = 0, v2 = 0;
+    cin >> v1 >> v2;
+    cout << "它们的乘积是：" << v1 * v2 << endl;
+    return 0;
+}
