57ffc6b3b930e9f9773379430861e209a3a38219
cekl <3515375434@qq.com>
2025-07-22 10:39:13 +0800

    reading Page 94

--- test.cpp
+++ test.cpp
@@ -3,7 +3,10 @@ using namespace std;
 
 
 int main(){
-    string global_str = "A is B"   "and B is A"; 
-    cout << global_str << endl; // 输出: A is Band B is A
+    int a = 0;
+    const int &b = a;
+    cout << b << endl; // Output: 0
+    a = 6;
+    cout << b << endl; 
     return 0;
 }
